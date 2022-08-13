81ebdba31caf011a5a713367ea3b65d02bf696c4
Your Name <you@example.com>
2022-08-13 20:00:49 +0400

    feat: latest stable version  index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arrExpr = Array.from(expr);
+  let newArr = [];
+  for (let i = 0; i < arrExpr.length; i+=10) {
+    const part = arrExpr.slice(i, i + 10);
+    newArr.push(part);
+  }
+  let longArr = newArr.map(function(elem){
+    let newArrSub = [];
+    for (let j = 0; j < elem.length; j+=2) {
+      const partSub = elem.slice(j, j + 2);
+      newArrSub.push(partSub.join(''));
+    }
+    return(newArrSub);
+  });
+  let arr = longArr.map(function(subElement){
+    let arrSub = [];
+    for (let l = 0; l < subElement.length; l++) {
+      if (subElement[l] === '00') {arrSub[l] = ''}
+      else {
+        if (subElement[l] === '10') {arrSub[l] = '.'}
+        else {
+          if (subElement[l] === '11') {arrSub[l] = '-'} else {arrSub[l] = '*'}
+        }
+      }
+    }
+    return(arrSub.join(''));
+  });
+  let res = arr.map(function(prop){
+    if (prop == '*****') {prop = ' '}
+    for (let key in MORSE_TABLE) {
+      if (prop==key) {prop = MORSE_TABLE[key]};
+    }
+    return(prop);
+  });
+  return(res.join(''));  // write your solution here
 }
 
 module.exports = {
